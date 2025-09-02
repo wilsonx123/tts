5abb7996212f1800ddfc41eb081274f6322a87f5
wangwangit001 <wangwangit001@gmail.com>
2025-09-02 16:34:18 +0800

    解决字符串带引号的问题!

--- index.js
+++ index.js
@@ -2033,7 +2033,20 @@ async function getAudioChunk(text, voiceName, rate, pitch, volume, style, output
     }
 }
 
+// XML文本转义函数
+function escapeXmlText(text) {
+    return text
+        .replace(/&/g, '&amp;')   // 必须首先处理 &
+        .replace(/</g, '&lt;')    // 处理 <
+        .replace(/>/g, '&gt;')    // 处理 >
+        .replace(/"/g, '&quot;')  // 处理 "
+        .replace(/'/g, '&apos;'); // 处理 '
+}
+
 function getSsml(text, voiceName, rate, pitch, volume, style, slien = 0) {
+    // 对文本进行XML转义
+    const escapedText = escapeXmlText(text);
+    
     let slien_str = '';
     if (slien > 0) {
         slien_str = `<break time="${slien}ms" />`
@@ -2041,7 +2054,7 @@ function getSsml(text, voiceName, rate, pitch, volume, style, slien = 0) {
     return `<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" version="1.0" xml:lang="zh-CN"> 
                 <voice name="${voiceName}"> 
                     <mstts:express-as style="${style}"  styledegree="2.0" role="default" > 
-                        <prosody rate="${rate}" pitch="${pitch}" volume="${volume}">${text}</prosody> 
+                        <prosody rate="${rate}" pitch="${pitch}" volume="${volume}">${escapedText}</prosody> 
                     </mstts:express-as> 
                     ${slien_str}
                 </voice> 
